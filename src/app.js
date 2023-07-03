a9c9c0f79d09bd94040baa233a427d36017224b7
andreguimaraesj <dev.detto@gmail.com>
2023-07-02 22:52:35 -0300

    test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from 'cors';
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 import joi from 'joi'
 import dotenv from 'dotenv';
 import DayJS from 'dayjs';
