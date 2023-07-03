67c7d67991998856bd555a2920c6076974e77e63
andreguimaraesj <dev.detto@gmail.com>
2023-07-02 21:17:49 -0300

    participants routes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,53 @@
+import express from "express";
+import cors from 'cors';
+import { MongoClient } from "mongodb";
+import joi from 'joi'
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+configDotenv.config();
+
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message))
+
+//PARTICIPANT ROUTES
+app.post('/participants', (req,res) =>{
+    const {name} = req.body;
+
+    const validation = joi.string().required().validate(name, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+    if (name/* VERIFICAR SE JÁ TEM O NOME NO BANCO */){
+        return res.status(409).send("Nome já em uso.")
+    }
+
+    db.collection("participants").insertOne({
+		name: name,
+		lastStatus: Date.now()
+	})
+        .then(participants => res.sendStatus(201))
+	    .catch(err => res.status(500).send(err.message))
+
+
+});
+
+
+app.get('participants', (req,res)=>{
+    const users = db.collection("participants").find().toArray()
+    .then(users => res.send(users))
+    .catch(err => res.status(500).send(err.message))
+});
+
  
+
+app.listen(5000);
